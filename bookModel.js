ead15b75919afa8d08337babb9c7cad652a2c147
Ethan Cratchley <138141942+EthanCratchley@users.noreply.github.com>
2024-04-09 13:39:14 -0700

    updates

--- bookModel.js
+++ bookModel.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
     title: String,
-    author: String,
+    author: [String],
+    year: Number,
+    pages: Number,
+    ISBN: String,
     coverImageUrl: String,
     amazonLink: String,
+    voteCount: Number,
     ratings: [Number],
+    price: Number
 })
 
 const Book = mongoose.model('Book', bookSchema);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Book = require('./bookModel');
+const axios = require('axios');
 
 // Initializing Express App and Connecting to MongoDB
 const app = express();
@@ -20,15 +21,41 @@ app.use(express.static('public'));
 // API Endpoints
 // Get list of Books
 app.get('/api/books', async (req, res) => {
-    const books = await Book.find({});
-    res.json(books);
+    try{
+        const books = await Book.find({});
+        res.json(books);
+    } catch (erro) {
+        console.error('Failed to fetch books', error);
+        res.status(500).json({message: 'Failed to fetch books'});
+    }
 });
 
 // Add a new book
 app.post('/api/books', async (req, res) => {
-    const newBook = new Book(req.body);
-    const savedBook = await newBook.save();
-    res.json(savedBook);
+    try {
+        const newBook = new Book(req.body);
+        const savedBook = await newBook.save();
+        res.json(savedBook);
+    } catch (error) {
+        console.error('Failed to save book:', error);
+        res.status(500).json({ message: 'Failed to save book' });
+    }
+});
+
+// Search books using Google Books API
+app.get('/search/books', async (req, res) => {
+    const query = req.query.q;
+    if (!query) {
+        return res.status(400).json({ message: 'Search query is required.' });
+    }
+  
+    try {
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.GOOGLE_BOOKS_API_KEY}`);
+        res.json(response.data.items || []);
+    } catch (error) {
+        console.error('Error fetching books from Google Books API:', error);
+        res.status(500).json({ message: 'Failed to fetch books from Google Books' });
+    }
 });
 
 // Start the Server
